16bf07f604ee402999493431836ba3c1a81cb542
chenmaxine <132473595+chenmaxine@users.noreply.github.com>
2023-05-04 23:09:36 +0800

    Delete utils directory

--- utils/request.js
+++
@@ -1,7 +0,0 @@
-export default async (options) => {
-	if (!options.url) {
-		return console.warn('请传入url');
-	}
-	const r = await uni.request(options);
-	return (r && r.data) ? r.data : (r[1] ? r[1].data : {});
-}
