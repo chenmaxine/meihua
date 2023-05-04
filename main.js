1d1bd65e269aff8c51e44a038c8aa22a5fae38c2
chenmaxine <132473595+chenmaxine@users.noreply.github.com>
2023-05-05 04:09:18 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+import App from './App';
+
+// #ifndef VUE3
+import Vue from 'vue';
+Vue.config.productionTip = false;
+App.mpType = 'app';
+
+const app = new Vue({
+  ...App
+});
+
+app.$mount();
+
+app.globalData = {
+  adInfo: {
+    jili: null
+  },
+  projectId: 'your_project_id', // 请用您的实际项目ID替换
+  canShowAd: false
+};
+
+app.getAdInfo = function () {
+  let _this = this;
+  uni.request({
+    url: `${config.API}api/w/getAdInfo`,
+    data: {
+      projectId: _this.globalData.projectId
+    },
+    method: 'POST',
+    success(res) {
+      _this.globalData.adInfo = res.data.data;
+      _this.globalData.canShowAd = res.data.data.open_ad == 1 ? true : false;
+      console.log(_this.globalData.adInfo);
+    }
+  });
+};
+
+app.getAdInfo();
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue';
+
+export function createApp() {
+  const app = createSSRApp(App);
+  return {
+    app
+  };
+}
+// #endif
+
---
+++ utils/config.js
@@ -0,0 +1,11 @@
+let API;
+
+if (process.env.NODE_ENV === 'development') {
+  API = 'https://app.beautyshe.life/';
+} else {
+  API = 'https://app.beautyshe.life/';
+}
+
+export default {
+  API
+};
